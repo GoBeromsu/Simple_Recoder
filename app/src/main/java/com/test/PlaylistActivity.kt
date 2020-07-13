b69c4ea4e63897688acb654d024d0ee5d1b4cd5e
Goberomsu <310o@naver.com>
2020-07-14 02:12:39 +0900

    list view 만드는 중

--- app/src/main/java/com/test/PlaylistActivity.kt
+++ app/src/main/java/com/test/PlaylistActivity.kt
@@ -1,31 +1,71 @@
 package com.test
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.playlist.*
+import android.widget.ListView
+import java.io.File
 
 // Play list 보여주는 activity
 class PlaylistActivity : AppCompatActivity() {
+
+    var mp3Path: String = Environment.getExternalStorageDirectory().absolutePath + "/Download/"
+    var listFiles = File(mp3Path).listFiles()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.playlist)
+//        var fileName: String? = null
+//        var extName: String? = null
+//        var mp3List = mutableListOf<String>()
+//
+//
+//        for (file in listFiles) {
+//            fileName = file.getName()
+//            extName = fileName.substring(-3)
+//            if (extName == "mp3") {
+//                mp3List.add(fileName)
+//            }
+//        }
+//
+
 
         button_back.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-
     }
-
-
-    // 클릭 시 해당하는 곡 삭제하는 함수
-    fun deleteSong() {}
-
-    // Download 아래에 있는 mp3 리스트트
-    fun showSong() {
-    }
-
-
+//
+//    private class mp3ListAdapter(context: Context) : BaseAdapter() {
+//
+//
+//        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+//            TODO("Not yet implemented")
+//        }
+//
+//        override fun getItem(p0: Int): Any {
+//            TODO("Not yet implemented")
+//        }
+//
+//        override fun getItemId(p0: Int): Long {
+//            TODO("Not yet implemented")
+//        }
+//
+//        override fun getCount(): Int {
+//            TODO("Not yet implemented")
+//        }
+//
+//
+//    }
 }
+
+
